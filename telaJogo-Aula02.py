3caf561bef0ecbaba057feeca39bf963fc03a5fa
DiogoRezen <98904037+DiogoRezen@users.noreply.github.com>
2022-02-07 21:37:42 -0300

    Codigos

---
+++ telaJogo-Aula02.py
@@ -0,0 +1,95 @@
+import turtle
+import os
+
+#draw screen
+screen = turtle.Screen()
+screen.title("My Pong")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.tracer(0)
+
+#draw paddle 1
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.color("white")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.penup()
+paddle_1.goto(-350,0)
+
+#draw paddle 2
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.color("white")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.penup()
+paddle_2.goto(350,0)
+
+#draw ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 1
+ball.dy = 1
+
+#head-up display
+hud = turtle.Turtle()
+hud.speed(0)
+hud.shape("square")
+hud.color("white")
+hud.penup()
+hud.hideturtle()
+hud.goto(0,260)
+hud.write("0 : 0", align="center", font=("Arial", 24, "normal"))
+
+#up paddle
+def paddle_1_up():
+    y = paddle_1.ycor()
+    if y < 250:
+        y += 30
+    else:
+        y = 250
+    paddle_1.sety(y)
+
+#down paddle
+def paddle_1_down():
+    y = paddle_1.ycor()
+    if y > -250:
+        y += -30
+    else:
+        y = -250
+    paddle_1.sety(y)
+
+#up paddle
+def paddle_2_up():
+    y = paddle_2.ycor()
+    if y < 250:
+        y += 30
+    else:
+        y = 250
+    paddle_2.sety(y)
+
+#down paddle
+def paddle_2_down():
+    y = paddle_2.ycor()
+    if y > -250:
+        y += -30
+    else:
+        y = -250
+    paddle_2.sety(y)
+
+# keyboard
+screen.listen()
+screen.onkeypress(paddle_1_up, "w")
+screen.onkeypress(paddle_1_down, "s")
+screen.onkeypress(paddle_2_up, "Up") #Up and Down são as setinhas do teclado -> todos os comentarios em inglês
+screen.onkeypress(paddle_2_down, "Down")
+
+#game loop
+while True:
+    screen.update()
+
